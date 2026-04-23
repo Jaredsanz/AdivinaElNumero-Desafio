04785d25c5b5b13653c31d3e0338dc71cfa2532c
Jared Sanchez <jesusjaredronal@gmail.com>
2026-04-23 01:31:54 -0600

    Correccion de ortografia

--- lib/modelos/nivel.dart
+++ lib/modelos/nivel.dart
@@ -6,13 +6,13 @@ class Nivel {
   Nivel({
     required this.nombre,
     required this.numeroMaximo,
-    required this.intentosMaximos
+    required this.intentosMaximos,
   });
 }
 
 final List<Nivel> niveles = [
-  Nivel(nombre: 'Facil', numeroMaximo: 10, intentosMaximos: 5),
-  Nivel(nombre: 'Normal', numeroMaximo: 20, intentosMaximos: 8),
-  Nivel(nombre: 'Dificil', numeroMaximo: 100, intentosMaximos: 15),
+  Nivel(nombre: 'Fácil', numeroMaximo: 10, intentosMaximos: 5),
+  Nivel(nombre: 'Medio', numeroMaximo: 20, intentosMaximos: 8),
+  Nivel(nombre: 'Avanzado', numeroMaximo: 100, intentosMaximos: 15),
   Nivel(nombre: 'Extremo', numeroMaximo: 1000, intentosMaximos: 25),
 ];
